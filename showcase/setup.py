b6d23d7c178a4db3ebd2a51260250b9dcd8b7565
Patrick <miraculixx@gmx.ch>
2020-10-13 21:08:00 +0200

    cleanup

--- showcase/setup.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from distutils.core import setup
-
-setup(name='showcase', version='1.0', description='simple omegaml hello world dash app', author='omegaml',
-      author_email='info@omegaml.io', url='http://omegaml.io', packages=['helloworld'], )
